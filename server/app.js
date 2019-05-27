cbbc2c095dfffa028f7f8403cc2a92497c762a81
Hiroki.T <hirodora@me.com>
2019-05-27 16:46:00 +0900

    add: create pod and send message

--- server/app.js
+++ server/app.js
@@ -7,17 +7,30 @@ const io = require('socket.io')(http);
 const PORT = 8765;
 app.use('/static', express.static('front/src'));
 
-app.get('/', function(request, response) {
-    response.sendFile(__dirname + '/front/index.html')
+
+app.get('/pod/*', function(request, response) {
+    response.sendFile(__dirname + '/front/pod.html');
 });
 
 
-http.listen(PORT, function() {
+http.listen(PORT, '0.0.0.0', function() {
     console.log('Server listening on ' + PORT);
-})
-
-
-io.on('connection', function(socket) {
-    console.log('Connected');
 });
 
+
+let store = {};
+io.on('connection', function (socket) {
+    let room_name = 'global';
+    console.log('new connection');
+    socket.on('join', function(msg) {
+        console.log('new join:');
+        socket.join(msg.pod_id);
+        socket.emit('join', 'SUCCESS');
+        io.sockets.in(msg.pod_id).emit("chat", "new user: " + msg.name);
+        room_name = msg.pod_id;
+    });
+
+    socket.on('chat', function(msg) {
+        socket.broadcast.to(room_name).emit('chat', msg);
+    });
+});
--- server/front/src/js/socket_talk.js
+++ server/front/src/js/socket_talk.js
@@ -1,1 +1,25 @@
-const socket = io()
+const pod_id = location.pathname.split('/').slice(-1)[0]
+const socket = io();
+
+// Join pod
+socket.emit('join', {'pod_id': pod_id, 'name': 'hiroki'});
+socket.on('join', function(msg){
+    console.log(msg);
+});
+
+
+let message = document.getElementById('message')
+let form = document.forms.chat;
+
+form.submit.addEventListener('click', function(e) {
+    e.preventDefault();
+    socket.emit('chat', form.content.value);
+    form.content.value = '';
+});
+
+
+socket.on('chat', function(msg) {
+    let li = document.createElement('li');
+    li.textContent = msg;
+    message.appendChild(li);
+});
