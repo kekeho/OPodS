694e3d38ceec01ce83a540d09d9ca323afcc7eb5
Hiroki.T <hirodora@me.com>
2019-05-30 02:02:59 +0900

    add: front design

--- front/static/js/socket_talk.js
+++ front/static/js/socket_talk.js
@@ -1,25 +1,31 @@
 const pod_id = location.pathname.split('/').slice(-1)[0];
+let myname = '';
 const socketio_address = 'http://' + location.hostname + ":8765";
 const socket = io(socketio_address);
+
+function autoscroll_timeline(element) {
+    element.scrollIntoView(false);
+    document.forms.chat.scrollIntoView(true);
+}
 
 // Join pod
 let loginform = document.forms.login;
 loginform.submit.addEventListener('click', function(e) {
     let login_msg = document.getElementById('login_message');
     login_msg.innerText = '';
-    const name = loginform.name.value;
+    myname = loginform.name.value;
     const password = loginform.password.value;
     e.preventDefault();
-    socket.emit('join', {'pod_id': pod_id, 'name': name, 'password': password});
+    socket.emit('join', {'pod_id': pod_id, 'name': myname, 'password': password});
     socket.on('join', function(msg){
         if (msg != 'SUCCESS'){
             login_msg.innerText = 'Something wrong';
         } else {
-            loginform.hidden = true;
-            login_msg.innerText = 'now you\'re in pod as ' + name;
+            loginform.parentNode.removeChild(loginform);  // delete self(loginform)
+            document.forms.chat.hidden = false;
         }
     });
-});
+}, {passive: false});
 
 
 let timeline = document.getElementById('timeline');
@@ -30,16 +36,43 @@ chatform.submit.addEventListener('click', function(e) {
     socket.emit('chat', message);
     chatform.content.value = '';
 
+    let username_span = document.createElement('span');
+    username_span.classList.add('name');
+    username_span.innerText = myname;
+
+    let content_span = document.createElement('span');
+    content_span.classList.add('content');
+    content_span.innerText = message;
+
     // add sent text to timeline
     let li = document.createElement('li');
-    li.textContent = message;
-    li.className = 'mymessage';
+    li.classList.add('mymessage', 'list-group-item');
+    li.appendChild(username_span);
+    li.appendChild(content_span);
     timeline.appendChild(li);
+
+    autoscroll_timeline(li);
+
+    chatform.content.focus();
 });
 
 
 socket.on('chat', function(msg) {
     let li = document.createElement('li');
-    li.textContent = '[' + msg.name + '] ' + msg.text;
+    li.classList.add('list-group-item');
+
+    let username_span = document.createElement('span');
+    username_span.classList.add('name');
+    username_span.innerText = msg.name;
+
+    let content_span = document.createElement('span');
+    content_span.classList.add('content');
+    content_span.innerText = msg.text;
+
+    li.appendChild(username_span);
+    li.appendChild(content_span);
+
     timeline.appendChild(li);
+
+    autoscroll_timeline(li);
 });
